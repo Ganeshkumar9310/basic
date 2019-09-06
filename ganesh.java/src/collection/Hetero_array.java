3200fe7a41862df83fc71d4c734e29452ff6ccc9
GANESH <GANESH@DESKTOP-C1BVNE6>
2019-09-06 10:29:02 -0700

    update

---
+++ ganesh.java/src/collection/Hetero_array.java
@@ -0,0 +1,15 @@
+package collection;
+
+public class Hetero_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Object hetro[] = {"ram",5,3.14,'a','e',true};
+		System.out.println(hetro[2]);
+		System.out.println(hetro[4]);
+		System.out.println(hetro[2].getClass());
+		System.out.println(hetro[4].getClass());
+
+	}
+
+}
