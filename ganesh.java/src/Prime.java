5cdebbe9e49ed564104975d020d9431988e2482c
Ganeshkumar9310 <54930843+Ganeshkumar9310@users.noreply.github.com>
2019-09-17 12:26:43 +0530

    Add files via upload

---
+++ ganesh.java/src/Prime.java
@@ -0,0 +1,32 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		int count=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				count++;
+			}
+		}
+		if(count==2)
+		{
+			System.out.println("PRIME NUMBER");
+		}
+		else
+		{
+			System.out.println("NOT A PRIME NUMBER");
+		}
+		sc.close();
+	}
+
+}
